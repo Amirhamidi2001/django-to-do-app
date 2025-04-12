ca121a47069a30cb0ced9feb5418e3090fb01a0d
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-04-12 16:32:09 +0330

    Register task in admin

--- core/todo/admin.py
+++ core/todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "completed")
+    list_filter = ("completed",)
+    search_fields = ("title",)
+
+
+admin.site.register(Task, TaskAdmin)
