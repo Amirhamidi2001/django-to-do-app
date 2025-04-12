3aeca0a5433577e25705fa93c07a4d2c1ccc7817
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-04-12 16:45:46 +0330

    Authentication Views

--- core/todo/views.py
+++ core/todo/views.py
@@ -1,22 +1,23 @@
 from django.views.generic import ListView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
 
 
-class TaskListView(ListView):
+class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = "todo/task_list.html"
     context_object_name = "tasks"
 
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     template_name = "todo/task_form.html"
     fields = ["title"]
     success_url = reverse_lazy("todo:task_list")
 
 
-class TaskUpdateView(UpdateView):
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
     template_name = "todo/task_update.html"
     fields = ["completed"]
