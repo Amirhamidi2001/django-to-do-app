f0fed02894cc3a2b29f3cff8b8c042edfc17fd6a
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-04-12 17:49:30 +0330

    Update views and templates

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskListView, TaskCreateView, TaskUpdateView
+from .views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 app_name = "todo"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", TaskListView.as_view(), name="task_list"),
     path("new/", TaskCreateView.as_view(), name="task_create"),
     path("update/<pk>/", TaskUpdateView.as_view(), name="task_update"),
+    path("delete/<pk>/", TaskDeleteView.as_view(), name="task_delete"),
 ]
--- core/todo/views.py
+++ core/todo/views.py
@@ -1,16 +1,24 @@
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
 
 
 class TaskListView(LoginRequiredMixin, ListView):
+    """
+    View that displays a list of tasks for authenticated users.
+    """
+
     model = Task
     template_name = "todo/task_list.html"
     context_object_name = "tasks"
 
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
+    """
+    View that provides a form to create a new task for authenticated users.
+    """
+
     model = Task
     template_name = "todo/task_form.html"
     fields = ["title"]
@@ -18,7 +26,20 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
 
 
 class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    """
+    View that provides a form to update an existing task for authenticated users.
+    """
+
     model = Task
     template_name = "todo/task_update.html"
     fields = ["completed"]
+    success_url = reverse_lazy("todo:task_list")
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    """
+    View that handles the deletion of a task for authenticated users.
+    """
+
+    model = Task
     success_url = reverse_lazy("todo:task_list")
